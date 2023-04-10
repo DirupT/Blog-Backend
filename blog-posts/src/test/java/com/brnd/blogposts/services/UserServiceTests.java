abaa8d2f7e94f9839108186b69b25e7a75e4ba71
dirupt <jjaguirre73@yahoo.com>
2023-04-10 00:31:42 -0500

    converted getUserById test to a unit test

--- blog-posts/src/test/java/com/brnd/blogposts/services/UserServiceTests.java
+++ blog-posts/src/test/java/com/brnd/blogposts/services/UserServiceTests.java
@@ -1,37 +1,40 @@
 package com.brnd.blogposts.services;
 
 import com.brnd.blogposts.models.User;
+import com.brnd.blogposts.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class UserServiceTests {
     @Mock
-    private UserService userService;
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserServiceImpl userService;
 
     @Test
     public void getUserById() {
         User user = new User("DirupT", "test");
 
-        when(userService.getUserById(1)).thenReturn(Optional.of(user));
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
 
-        Optional<User> fetchedUser = userService.getUserById(1);
+        Optional<User> fetchedUser = userService.getUserById(1L);
 
         assertTrue(fetchedUser.isPresent());
         assertEquals(user, fetchedUser.get());
 
-        Optional<User> nonExistentUser = userService.getUserById(999);
+        Optional<User> nonExistentUser = userService.getUserById(999L);
         assertFalse(nonExistentUser.isPresent());
     }
 }
